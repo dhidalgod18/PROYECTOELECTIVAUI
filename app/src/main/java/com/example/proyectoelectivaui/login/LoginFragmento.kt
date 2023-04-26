219e8cecdc2c9203d94a1844a809bb8aedcf8a92
Daniela Hidalgo <hdanielad2001@gmail.com>
2023-04-25 21:23:06 -0500

    -Se agregaron las libreria para poder usar la base de datos local sqlLite( En el plugins se adiciono  id 'kotlin-kapt', kapt "androidx.room:room-compiler:2.5.1",  implementation "androidx.room:room-runtime:2.5.1",  implementation "androidx.room:room-ktx:2.5.1") -se agrego la clase usuarioEntity(Donde se crea el nombre de la tabla en la base de datos) y se agregan los atributos del usuario. -se agrego la clase usuarioDAO(contiene los metodos para interactuar con la base de datos) -se agrego la clase usuarioDataBase(contiene el constructor de la clase y el nombre de la base de datos que se va a crear localmente) -se agrego la clase RegistroViewModel(Contiene el metodo agregar, el cual utiliza los objeto de entity, dao y la database) -se agrego en el fragmentoregistro(contiene la clase viewModel y captura los datos del layout pasandoselo al metodo agregar de la clase RegistroViewModel) -Se realizo el cambio del id del boton registro en el layout login

--- app/src/main/java/com/example/proyectoelectivaui/login/LoginFragmento.kt
+++ app/src/main/java/com/example/proyectoelectivaui/login/LoginFragmento.kt
@@ -1,4 +1,4 @@
-package com.example.proyectoelectivaui
+package com.example.proyectoelectivaui.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.proyectoelectivaui.R
 import com.example.proyectoelectivaui.databinding.FragmentLoginFragmentoBinding
 
 
