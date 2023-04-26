cbae96b5bcc3af2ba045d5f4fd2fd36151b6f05d
14DAVEN18 <32316133+14DAVEN18@users.noreply.github.com>
2023-04-25 22:27:45 -0500

    Revert "Updates"

--- app/src/main/java/com/example/proyectoelectivaui/LoginFragmento.kt
+++ app/src/main/java/com/example/proyectoelectivaui/LoginFragmento.kt
@@ -1,6 +1,5 @@
 package com.example.proyectoelectivaui
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -22,8 +21,7 @@ class LoginFragmento : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_login_fragmento, container, false)
-        this._binding = FragmentLoginFragmentoBinding.inflate(inflater, container, false)
-        this.binding.btnLogin.setOnClickListener{ login() }
+        _binding = FragmentLoginFragmentoBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -39,15 +37,6 @@ class LoginFragmento : Fragment() {
         }*/
     }
 
-
-    // In Fragment 1 in Activity 1
-    fun login() {
-        val loginIntent = Intent(activity, MainActivity2::class.java)
-        loginIntent.putExtra("user", "David")
-        startActivity(loginIntent)
-
-        // VALIDAR CREDENCIALES
-    }
 
 
 }
